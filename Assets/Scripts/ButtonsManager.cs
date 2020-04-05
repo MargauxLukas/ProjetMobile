4377e07a271f9cf73877727fac65d72958665fc4
Lukas Margaux <m.lukas@rubika-edu.com>
2020-04-05 14:40:06 +0200

    Button Manager WIP

---
+++ Assets/Scripts/ButtonsManager.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonsManager : MonoBehaviour
+{
+    public void Cut()
+    {
+        Debug.Log("Cut");
+
+        //Proto 1
+        
+
+        //Proto 2
+
+    }
+
+    public void Knead()
+    {
+        Debug.Log("Knead");
+
+        //Proto 1
+
+
+        //Proto 2
+
+    }
+
+    public void Whip()
+    {
+        Debug.Log("Whip");
+
+        //Proto 1
+
+
+        //Proto 2
+
+    }
+
+    public void Eat()
+    {
+        Debug.Log("Eat");
+
+        //Proto 1
+
+
+        //Proto 2
+
+    }
+}
