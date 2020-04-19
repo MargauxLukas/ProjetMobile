842cc46b5b0900cf2b5101f20a9d6fc9977984b6
Lukas Margaux <m.lukas@rubika-edu.com>
2020-04-19 16:03:43 +0200

    Tokens Manager WIP

---
+++ Assets/Scripts/Wallet.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class Wallet : MonoBehaviour
+{
+    public static Wallet instance;
+    public int tokens = 0;
+
+    private void Start()
+    {
+        if(instance = null)
+        {
+            instance = this;
+        }
+        else if (instance != this)
+        {
+            Destroy(this);
+            return;
+        }
+
+        DontDestroyOnLoad(this);
+        UpdateWallet();
+    }
+
+    private void Update()
+    {
+        Debug.Log("Update");
+        SceneManager.sceneLoaded += OnSceneLoaded;
+    }
+
+    void OnSceneLoaded(Scene scene, LoadSceneMode mode)
+    {
+        Debug.Log("Loaded : " + scene.name);
+        Debug.Log(mode);
+
+        if (scene.buildIndex == 0)
+        {
+            UpdateWallet();
+        }
+    }
+
+    void UpdateWallet()
+    {
+        Debug.Log("Wallet");
+        GameObject tokensText = GameObject.FindGameObjectWithTag("Tokens");
+        Debug.Log(tokensText.name);
+
+        if(tokens >= 999)
+        {
+            tokens = 999;
+        }
+
+        tokensText.GetComponent<Text>().text = tokens.ToString();
+        Debug.Log(tokensText.GetComponent<Text>().text);
+    }
+}
