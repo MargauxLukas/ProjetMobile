e216bbb26e6f6fddb1b3843a08d672212fe9fa3a
Alexandre COURTIN <aoyama_masaya@hotmail.fr>
2020-05-03 16:42:05 +0200

    Add : Reset Save and CheatCode

---
+++ Assets/Scripts/DangerZone.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DangerZone : MonoBehaviour
+{
+    public InputField field;
+    private string code = "UNLOCKALL";
+
+    public void VerifyCode()
+    {
+        if (field.text.ToUpper() == code)
+        {
+            UnlockAll();
+        }
+    }
+
+    void UnlockAll()
+    {
+        PlayerPrefs.SetInt("level", 6);
+        Application.Quit();
+    }
+
+    public void ResetSave()
+    {
+        PlayerPrefs.DeleteAll();
+        Application.Quit();
+    }
+}
--- Assets/Scripts/ToolsHeader.cs
+++ Assets/Scripts/ToolsHeader.cs
@@ -3,17 +3,20 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class LockLevel : MonoBehaviour
+public class ToolsHeader : MonoBehaviour
 {
+    public Color32 color;
+    public Color32 white = new Color32(255, 255, 255, 255);
     [SerializeField] int levelRequirement;
-    // Start is called before the first frame update
-    void Start()
+
+    private void Start()
     {
         int currentLevel = PlayerPrefs.GetInt("level");
+
         //Debug.Log(currentLevel + " >= " + levelRequirement);
         if (currentLevel >= levelRequirement)
         {
-            GetComponent<Button>().interactable = true;
+            GetComponent<Image>().color = color;
         }
     }
 }
