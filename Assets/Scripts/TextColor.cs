e22f71ef76ca8cd28ea674a98adb9a465fdaed76
Lukas Margaux <m.lukas@rubika-edu.com>
2020-06-09 23:05:38 +0200

    New colors on old buttons

---
+++ Assets/Scripts/TextColor.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TextColor : MonoBehaviour
+{
+    public Color32 normalColor;
+    public Color32 pressedColor;
+
+
+    public void PressedColor(Text txt)
+    {
+        txt.color = pressedColor;
+    }
+
+    public void NormalColor(Text txt)
+    {
+        txt.color = normalColor;
+    }
+}
